24795d21a5bee269580f52517b3ff1364646105e
bookstack <bookstack@b5c01dbd-2b39-0410-97b0-3bdaba453c0f>
2007-09-02 21:04:21 +0000

    Detail view is ready.

--- trunk/gelman/library/views.py
+++ trunk/gelman/library/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+from django.shortcuts import render_to_response, get_object_or_404
+from gelman.library.models import Book
+
+def detail(request, isbn):
+	book = get_object_or_404(Book, isbn13=isbn)
+	return render_to_response('books/detail.html', {'book': book})
+
