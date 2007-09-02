da1d4f242850847e38eb716fba78c87b7c5ed00f
bookstack <bookstack@b5c01dbd-2b39-0410-97b0-3bdaba453c0f>
2007-09-02 20:38:11 +0000

    Setup the model, now we can login into the admin page. Username: bookstack Password: gelman

--- trunk/gelman/library/models.py
+++ trunk/gelman/library/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
+# NOTE: maxlength is obsolete in >0.96, replaced by max_length
+
+class Author(models.Model):
+	# Refactor me as first, last
+	name = models.CharField(maxlength=64)
+	
+	def __str__(self):
+		return self.name
+
+	class Admin:
+		pass
+
+
+class Publisher(models.Model):
+	name = models.CharField(maxlength=64)
+
+	def __str__(self):
+		return self.name
+
+	class Admin:
+		pass
+
+
+class Book(models.Model):
+	isbn13 = models.CharField(maxlength=13, primary_key=True)
+	name = models.CharField(maxlength=255)
+	authors = models.ManyToManyField(Author)
+	pages = models.IntegerField()
+	publisher = models.ForeignKey(Publisher)
+	pub_date = models.DateTimeField('data published')
+
+	def __str__(self):
+		return self.name
+
+	class Admin:
+		pass
+
 # Create your models here.
