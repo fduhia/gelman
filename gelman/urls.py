763eb6c6a0c28d0bb909d3bc5a8b581777caa60f
bookstack <bookstack@b5c01dbd-2b39-0410-97b0-3bdaba453c0f>
2007-09-02 21:04:21 +0000

    Detail view is ready.

--- gelman/urls.py
+++ gelman/urls.py
@@ -4,5 +4,6 @@ urlpatterns = patterns('',
     # Example:
     # (r'^gelman/', include('gelman.foo.urls')),
 
+    (r'^library/(?P<isbn>\d+)/$', 'gelman.library.views.detail'),
     (r'^admin/', include('django.contrib.admin.urls')),
 )
