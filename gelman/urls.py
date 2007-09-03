e0b9f0bbfaf563d350177b6ecaf41366c3b0109e
bookstack <bookstack@b5c01dbd-2b39-0410-97b0-3bdaba453c0f>
2007-09-03 07:29:37 +0000

    Add AJAX AWS support.

--- gelman/urls.py
+++ gelman/urls.py
@@ -6,5 +6,6 @@ urlpatterns = patterns('',
 
     (r'^library/(?P<isbn>\d+)/$', 'gelman.library.views.detail'),
 	(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/home/bookstack/projects/media'}),
+    (r'^admin/library/book/add', 'gelman.library.admin_views.book_add'),
     (r'^admin/', include('django.contrib.admin.urls')),
 )
