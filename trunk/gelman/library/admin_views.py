aab42431e0b9e3d46bacbabcecd862cf3a783521
bookstack <bookstack@b5c01dbd-2b39-0410-97b0-3bdaba453c0f>
2007-09-03 07:29:37 +0000

    Add AJAX AWS support.

---
+++ trunk/gelman/library/admin_views.py
@@ -0,0 +1,9 @@
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+from django.contrib.admin.views.decorators import staff_member_required
+from gelman.library.models import Book
+
+@staff_member_required
+def book_add(request):
+	return render_to_response( "admin/library/book/add.html");
+
