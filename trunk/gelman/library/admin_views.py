4b5e3a58b5c66ce9b0d64c6a01c119015321f5d4
bookstack <bookstack@b5c01dbd-2b39-0410-97b0-3bdaba453c0f>
2007-09-06 02:12:49 +0000

    Add simplejson.

--- trunk/gelman/library/admin_views.py
+++ trunk/gelman/library/admin_views.py
@@ -1,6 +1,7 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.contrib.admin.views.decorators import staff_member_required
+from django.utils import simplejson
 from gelman.library.models import Book
 
 @staff_member_required
@@ -9,4 +10,10 @@ def book_add(request):
 		return render_to_response( "admin/library/book/add.html", {'request': request});
 
 	# Handle the POST action
+	items = simplejson.loads(request.POST['items'])
+	
+	# Insert it into the database
+	import pdb
+	pdb.set_trace()
+
 
