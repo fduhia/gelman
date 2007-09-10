12384d77b829f959c9880b96f7c0b14d14069617
bookstack <bookstack@b5c01dbd-2b39-0410-97b0-3bdaba453c0f>
2007-09-10 22:03:33 +0000

    Replace isbn13 by isbn for the latest model refactory Add index to handle the /library front page.

--- gelman/library/views.py
+++ gelman/library/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from gelman.library.models import Book
 
+def index(request):
+	recently_released = {
+		'headline': "Recently Released",
+		'item':  Book.objects.latest('pub_date')
+	}
+	return render_to_response('books/index.html',{
+		'section_list': [recently_released], 
+		'title': 'Gelman'})
+
+
 def detail(request, isbn):
-	book = get_object_or_404(Book, isbn13=isbn)
+	book = get_object_or_404(Book, isbn=isbn)
 	return render_to_response('books/detail.html', {'book': book})
 
--- gelman/urls.py
+++ gelman/urls.py
@@ -4,7 +4,8 @@ urlpatterns = patterns('',
     # Example:
     # (r'^gelman/', include('gelman.foo.urls')),
 
-    (r'^library/(?P<isbn>\d+)/$', 'gelman.library.views.detail'),
+    (r'^library/(?P<isbn>\d+[xX]?)/$', 'gelman.library.views.detail'),
+    (r'^library/$', 'gelman.library.views.index'),
 	(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/home/bookstack/projects/media'}),
     (r'^admin/library/book/add', 'gelman.library.admin_views.book_add'),
     (r'^admin/', include('django.contrib.admin.urls')),
