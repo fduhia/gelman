c9a4638523e1c2c47ecbdad676449a9d9e7b3cb6
bookstack <bookstack@b5c01dbd-2b39-0410-97b0-3bdaba453c0f>
2007-09-19 00:59:21 +0000

    Reorganize the data scheme. Move add.html to add-by-search.html subclass change-form.html `

--- trunk/gelman/library/models.py
+++ trunk/gelman/library/models.py
@@ -25,16 +25,39 @@ class Publisher(models.Model):
 
 class Book(models.Model):
 	isbn = models.CharField(maxlength=10, primary_key=True)
-	name = models.CharField(maxlength=255)
+	title = models.CharField(maxlength=255)
 	authors = models.ManyToManyField(Author)
 	pages = models.IntegerField()
 	publisher = models.ForeignKey(Publisher)
-	pub_date = models.DateField('data published')
+	pub_date = models.DateField()
+	timestamp = models.DateTimeField()
+	thumb = models.ImageField(upload_to="images")
+	cover = models.ImageField(upload_to="images")
 
 	def __str__(self):
-		return self.name
+		return self.title
 
 	class Admin:
 		pass
 
-# Create your models here.
+
+class FileType(models.Model):
+	type = models.CharField(maxlength=10)
+
+	def __str__(self):
+		return self.type
+
+	class Admin:
+		pass
+
+
+class File(models.Model):
+	type = models.ForeignKey(FileType)
+	handle = models.FileField(upload_to="files")
+	meta = models.ForeignKey(Book)
+	
+	def __str__(self):
+		return self.get_handle_url()
+
+	class Admin:
+		pass
