d6a516597112c11b3d6df4286a0debe5e11c2a97
bookstack <bookstack@b5c01dbd-2b39-0410-97b0-3bdaba453c0f>
2007-09-19 20:10:57 +0000

    Finish the detail view.

--- trunk/gelman/urls.py
+++ trunk/gelman/urls.py
@@ -1,23 +1,17 @@
 from django.conf.urls.defaults import *
-from gelman.library.models import Book
-
-cursor = {
-	'queryset': Book.objects.all(),
-}
 
 urlpatterns = patterns('',
 	# static pages:
 	(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/home/bookstack/projects/gelman/trunk/gelman/media'}),
-	(r'^repo/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/home/share/eBooks'}),
+	(r'^repo/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/home/share/gelman'}),
 
 	# login, logout
 	(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html', }),
 	(r'^accounts/logout/$', 'gelman.library.views.logout_view', ),
-
+	# admin
     (r'^admin/library/book/add-by-search', 'gelman.library.admin_views.book_add_by_search'),
     (r'^admin/', include('django.contrib.admin.urls')),
 
 	# library
-    (r'^library/$', 'gelman.library.views.index'),
-    (r'^library/(?P<isbn>\d+[xX]?)/$', 'gelman.library.views.detail'),
+    (r'^library/', include('gelman.library.urls')),
 )
